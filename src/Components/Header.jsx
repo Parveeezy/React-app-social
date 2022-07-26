298edb4476e9105bfbaa6daa352488768d870858
Parviz Shirinov <parveeezy@gmail.com>
2022-07-26 16:33:53 +0300

    added alt to images

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = () => {
     return (
         <header className="header">
-            <img src="https://seeklogo.com/images/S/svg-logo-C2C6DE140E-seeklogo.com.png"/>
+            <img alt="img" src="https://seeklogo.com/images/S/svg-logo-C2C6DE140E-seeklogo.com.png"/>
         </header>
     );
 };
