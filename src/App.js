299a0dc6780550f2d48067ad80389e9711f3af73
Parviz Shirinov <parveeezy@gmail.com>
2022-07-28 19:01:56 +0300

    Added component Dialogs

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@ import './App.css';
 import Header from "./Components/Header/Header";
 import Navbar from "./Components/Navbar/Navbar";
 import Profile from "./Components/Profile/Profile";
-
+import Dialogs from "./Components/Dialogs/Dialogs";
 
 const App = (props) => {
     return (
-        <div className="App-wrapper">
+        <div className="app-wrapper">
             <Header/>
             <Navbar/>
-            <Profile/>
+
+            <div className="app-wrapper-content">
+                {/*<Profile/>*/}
+                <Dialogs/>
+            </div>
         </div>
+
     );
 }
 
--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -3,7 +3,7 @@ import MyPosts from "./MyPosts/MyPosts";
 
 const Profile = () => {
     return (
-        <div className={s.profile}>
+        <div>
             <div>
                 <img alt="img" src="https://nissi-beach.com/wp-content/uploads/2016/11/NissiBeach-21-1620x600.jpg"/>
             </div>
