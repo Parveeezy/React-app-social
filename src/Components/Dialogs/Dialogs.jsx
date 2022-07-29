7ef8b6fef4fa04dfd0794509ec96e7247619508f
Parviz Shirinov <parveeezy@gmail.com>
2022-07-29 17:25:30 +0300

    Add to Dialogs users and messages

--- src/Components/Dialogs/Dialogs.jsx
+++ src/Components/Dialogs/Dialogs.jsx
@@ -2,12 +2,27 @@ import classes from './Dialogs.module.css'
 
 const Dialogs = (props) => {
     return (
-        <div>
-            <div>
-
+        <div className={classes.dialogs}>
+            <div className={classes.dialogsItems}>
+                <div className={classes.dialogsItem + classes.active}>
+                    Parviz
+                </div>
+                <div className={classes.dialogsItem}>
+                    Nisso
+                </div>
+                <div className={classes.dialogsItem}>
+                    Parisa
+                </div>
+                <div className={classes.dialogsItem}>
+                    Latif
+                </div>
             </div>
-            <div>
 
+            <div className={classes.dialogsMessages}>
+                <div className={classes.dialogsMessage}>Hello</div>
+                <div className={classes.dialogsMessage}>One</div>
+                <div className={classes.dialogsMessage}>Two</div>
+                <div className={classes.dialogsMessage}>Run</div>
             </div>
         </div>
     );
