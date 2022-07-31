698413155d1033d297186abf7ff42fb6a5b93470
Parveeezy <parveeezy@gmail.com>
2022-07-31 22:27:56 +0300

    git email test

--- src/Components/Profile/MyPosts/MyPosts.jsx
+++ src/Components/Profile/MyPosts/MyPosts.jsx
@@ -2,7 +2,7 @@ import Post from "./Post/Post";
 import classes from "./MyPosts.module.css";
 
 const MyPosts = (props) => {
-    
+
     let postsElements = props.postsData
         .map(post => <Post message={post.message} likesCount={post.likesCount}/>)
 
@@ -13,6 +13,7 @@ const MyPosts = (props) => {
                 <div>
                     <textarea></textarea>
                 </div>
+                
                 <div>
                     <button>Add post</button>
                 </div>
