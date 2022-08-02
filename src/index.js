afe256617ddb3cbd581ad50e80bff8002ca49f9a
Parviz Shirinov <parveeezy@gmail.com>
2022-08-02 13:54:05 +0300

    fix BrowserRouter position in index.js

--- src/index.js
+++ src/index.js
@@ -7,10 +7,10 @@ import state from "./redux/state";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <BrowserRouter>
+        <React.StrictMode>
             <App state={state}/>
+        </React.StrictMode>
     </BrowserRouter>
-  </React.StrictMode>
 );
 
