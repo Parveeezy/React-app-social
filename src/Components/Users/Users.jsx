ab0a6c195fe36c48e185317d6268623785923626
Parveeezy <parveeezy@gmail.com>
2022-08-21 01:23:34 +0300

    some fix

--- src/Components/Users/Users.jsx
+++ src/Components/Users/Users.jsx
@@ -4,15 +4,20 @@ import axios from "axios";
 import userPhoto from '../../assets/images/user_img.png'
 
 const Users = (props) => {
-    if (props.users.length === 0) {
-        axios
-            .get('https://social-network.samuraijs.com/api/1.0/users')
-            .then(response => {
-                props.setUsers(response.data.items)
-            })
+
+    const getUsers = () => {
+        if (props.users.length === 0) {
+            axios
+                .get('https://social-network.samuraijs.com/api/1.0/users')
+                .then(response => {
+                    props.setUsers(response.data.items)
+                })
+        }
     }
 
+
     return <div>
+        <button onClick={getUsers}>Get Users</button>
         {
             props.users.map(user =>
                 <div key={user.id}>
