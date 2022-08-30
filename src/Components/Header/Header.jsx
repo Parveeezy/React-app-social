708b5522d26aad0ecc2a4bcedae61cdbf2fe6f5d
Parviz Shirinov <parveeezy@gmail.com>
2022-08-30 17:49:45 +0300

    possibility of authorization

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,9 +1,15 @@
 import s from "./Header.module.css"
+import {NavLink} from "react-router-dom";
 
-const Header = () => {
+const Header = (props) => {
     return (
         <header className={s.header}>
             <img alt="img" src="https://seeklogo.com/images/S/svg-logo-C2C6DE140E-seeklogo.com.png"/>
+
+            <div className={s.loginBlock}>
+                {props.isAuth ? props.login: <NavLink to={'/login'}>Login</NavLink>
+                }
+            </div>
         </header>
     );
 };
